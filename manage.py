d84c699073c0a18bb244a9b88ead35622438c58d
KevInZhao <hellozhaowenkai@gmail.com>
2021-04-13 11:09:30 +0800

    build(root): update project architecture

---
+++ manage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Dotfiles' command-line utility for administrative tasks."""
+
+
+def main():
+    """Run administrative tasks."""
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
